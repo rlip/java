755908a12845fa5dcc00ab925d84023c74980c49
liproman@gmail.com <liproman@gmail.com>
2021-10-28 17:21:41 +0200

    react, designpatterns, testjunit

--- testjunit/src/main/java/electricity/ElectricityMeter.java
+++ testjunit/src/main/java/electricity/ElectricityMeter.java
@@ -22,6 +22,34 @@ public class ElectricityMeter {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ElectricityMeter)) return false;
+
+        ElectricityMeter that = (ElectricityMeter) o;
+
+        if (Float.compare(that.kwh, kwh) != 0) return false;
+        if (centsForKwh != that.centsForKwh) return false;
+        if (tariffOn != that.tariffOn) return false;
+        if (Float.compare(that.kwhTariff, kwhTariff) != 0) return false;
+        if (centsForTariff != that.centsForTariff) return false;
+        if (electricityTariffStartHour != that.electricityTariffStartHour) return false;
+        return electricityTariffEndHour == that.electricityTariffEndHour;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (kwh != +0.0f ? Float.floatToIntBits(kwh) : 0);
+        result = 31 * result + centsForKwh;
+        result = 31 * result + (tariffOn ? 1 : 0);
+        result = 31 * result + (kwhTariff != +0.0f ? Float.floatToIntBits(kwhTariff) : 0);
+        result = 31 * result + centsForTariff;
+        result = 31 * result + electricityTariffStartHour;
+        result = 31 * result + electricityTariffEndHour;
+        return result;
+    }
+
     private boolean isTariffNow() {
         Calendar rightNow = Calendar.getInstance();
         int hour = rightNow.get(Calendar.HOUR_OF_DAY);
